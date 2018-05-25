65241b81424e19f6241fd406035e3cd247c81fbe
charif104 <bad_boy104@hotmail.com>
2018-05-25 13:05:40 +0200

    Added comments to PWM config

--- johnson/johnson/src/config/PWM_Configuration.h
+++ johnson/johnson/src/config/PWM_Configuration.h
@@ -2,14 +2,19 @@
  * PWM_Configuration.h
  *
  * Created: 2018-05-02 12:10:34
- *  Author: ziko205i
+ *  Author: Charif Ramadan
  */ 
 
 
 #ifndef PWM_CONFIGURATION_H_
 #define PWM_CONFIGURATION_H_
 
-void PWM_init(void);
+ // Definera pin 6 och pin 7 som en pwm pinne
+ 
+#define PIN_24  IOPORT_CREATE_PIN(PIOC, 24)  // pinne 6
+#define PIN_23  IOPORT_CREATE_PIN(PIOC, 23)  // pinne 7
+
+void pwm_setup(void);
 void initPin6(void);
 void initPin7(void);
 void pwm_pin_6(uint32_t duty);
